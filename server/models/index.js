168cad22106ab044288ca8924790aadb4efa2570
cys2110 <csheridan2110@gmail.com>
2024-03-08 14:57:57 -0500

    user model

--- server/models/index.js
+++ server/models/index.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose')
 const LocationSchema = require('./Location')
 const TypeSchema = require('./Type')
 const VenueSchema = require('./Venue')
+const UserSchema = require('./User')
 
 const Location = mongoose.model('Location', LocationSchema)
 const Type = mongoose.model('Type', TypeSchema)
 const Venue = mongoose.model('Venue', VenueSchema)
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {
     Location,
     Type,
-    Venue
+    Venue,
+    User
 }
