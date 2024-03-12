223709a883729a5aea43063fc6e6abdd53322ec6
maddiemaz <maddiemaz18@gmail.com>
2024-03-12 12:25:48 -0400

    results & venue detail pages

--- client/src/components/Main/Venue.jsx
+++ client/src/components/Main/Venue.jsx
@@ -1,33 +1,50 @@
 import {useNavigate} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Venue (props) {
     let navigate = useNavigate()
 
-    const showVenue = (venue) => {
-        navigate(`${venue.id}`)
+    const showVenue = (id) => {
+        navigate(`${id}`)
     }
     
-    return (
-        <div className="venue-list-page">
-            <div className="search-list-title">Venues</div>
-            <div className="search-list-grid">
-                {props.venues.map((venue) => (
-                    <div className="search-list-card">
-                        {/* <img src={venue.img} alt={venue.name} className="list-card-image"/> */}
-                        <div className="list-card-title">{venue.city}</div>
-                        <div className="list-card-subtitle">{venue.location}</div>
-                        <button className="list-card-button-dropdown-open"/>
-                        {/* figure out how to show/hide dropdown info onClick */}
-                        <div className="list-card-dropdown">
-                            <div className="list-card-dropdown-primary">{venue.type}</div>
-                            <div className="list-card-dropdown-secondary">{venue.owner}</div>
-                            <div className="list-card-dropdown-primary">{venue.price}</div>
-                            <button className="list-card-button-dropdown-close"/>
+    const [venues, setVenues] = useState([])
+
+    useEffect(() => {
+        const getVenues = async() => {
+            const response = await axios.get('http://localhost:3001/venues')
+            setVenues(response.data)
+        }
+        getVenues()
+    }, [])
+    console.log(venues)
+    
+    if (!venues) {
+        return <div className="loading">Loading...</div>
+    } else {
+        return (
+            <div className="venue-list-page">
+                <div className="search-list-title">Venues</div>
+                <div className="search-list-grid">
+                    {venues.map((venue, id) => (
+                        <div className="search-list-card">
+                            {/* <img src={venue.img} alt={venue.name} className="list-card-image"/> */}
+                            <div className="text-title-20">{venue.city}</div>
+                            <div className="text-caps-12">{venue.location}</div>
+                            <button className="list-card-button-dropdown-open"/>
+                            {/* figure out how to show/hide dropdown info onClick */}
+                            <div className="list-card-dropdown">
+                                <div className="text-standard-16">{venue.type}</div>
+                                <div className="text-standard-12">{venue.owner}</div>
+                                <div className="text-standard-14">{venue.price}</div>
+                                <button className="list-card-button-dropdown-close"/>
+                            </div>
+                            <button className="list-card-button-explore-venue" onClick={() => showVenue(venue.id)} key={id}>explore</button>
                         </div>
-                        <button className="list-card-button-venue" onClick={() => showVenue(venue)} key={venue.id}>explore</button>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
