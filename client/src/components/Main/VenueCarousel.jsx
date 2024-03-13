0a73e28c7f4be797b9c6b22af0b45c11abd0da34
itzel <itzelalwarafii@gmail.com>
2024-03-13 09:35:45 -0700

    venueGalleryCarousel

---
+++ client/src/components/Main/VenueCarousel.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+export default function VenueCarousel() {
+
+    const [venue, setVenue] = useState(null)
+    const { id } = useParams()
+
+    useEffect(() => {
+        const getVenue = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/venues/${id}`)
+                setVenue(response.data)
+            } catch (error) {
+                console.error('Error fetching venue:', error)
+            }
+        }
+        getVenue()
+    }, [id])
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    }
+
+    if (venue) {
+        return (
+            <div className="VenueGalleryContainer">
+                {venue.img.length > 0 ? (
+                    <Slider {...settings}>
+                        {venue.img.map((imgUrl, imgIndex) => (
+                            <img key={`img-${imgIndex}`} className='Venuegalleryimg' src={imgUrl} alt={`img-${imgIndex}`} />
+                        ))}
+                    </Slider>
+                ) : (
+                    <div className="no-images">No images available</div>
+                )}
+            </div>
+        )
+    } else {
+        return <div className="loading">Loading...</div>
+    }
+}
